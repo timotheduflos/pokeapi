cba49a826a212b8cde8a8b25eddd796033e03951
timotheduflos <timotheduflos@gmail.com>
2023-01-12 16:17:03 +0100

    testPush

---
+++ PokeApi/src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="bg-red-900 h-12 text-white flex justify-center items-center">
+        Hello
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
